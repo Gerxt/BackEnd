6f74572ab038d536eacbefa2289f4b4ed690de9c
Gerxt <german-2396@hotmail.com>
2024-05-15 16:25:34 -0500

    adding mokito methods

--- src/test/java/culturemedia/service/CultureMediaServiceTest.java
+++ src/test/java/culturemedia/service/CultureMediaServiceTest.java
@@ -2,10 +2,13 @@ package culturemedia.service;
 import culturemedia.repository.VideoRepository;
 import culturemedia.repository.ViewsRepository;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 
 import culturemedia.model.Video;
 
@@ -14,12 +17,20 @@ import culturemedia.repository.impl.*;
 
 import culturemedia.exception.VideoNotFoundExeption;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import culturemedia.service.CultureMediaService;
 import culturemedia.service.impl.CultureMediaServiceImpl;
+
+
+import culturemedia.repository.*;
+
+
+
 
 class CultureMediaServiceTest 
 {
-
+    private VideoRepository videoRepository = Mockito.mock();
     private CultureMediaService cultureMediaService;
 
     Video VideoA = new Video("100", "The Lion King", "_ _ _ _ _", 2.2);
@@ -32,85 +43,86 @@ class CultureMediaServiceTest
    
     @BeforeEach
     void init() {
-        VideoRepository videoRepository = new VideoRepositoryImpl();
+        
         ViewsRepository viewsRepository = new ViewsRepositoryImpl();
         cultureMediaService = new CultureMediaServiceImpl(videoRepository, viewsRepository);
 
-       
 
     }
 
+    private void mockFindAll(List <Video> videos){
+        doReturn(videos)
+        .when(videoRepository)
+        .findAll();
+    }
+
+    private void mockFind(String title, List<Video> videos){
+        doReturn(videos)
+        .when(videoRepository)
+        .find(eq(title));
+    }
+
+    private void mockFindTime(Double fromDuration, Double toDuration, List<Video> videos){
+        doReturn(videos)
+        .when(videoRepository)
+        .find(eq(fromDuration), eq(toDuration));
+    }
+
+    
+
     @Test
     void when_FindAll_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
-        assertThrows(VideoNotFoundExeption.class, () -> {
-            cultureMediaService.ListAllVideos();
+        mockFindAll(Collections.emptyList());
+        assertThrows(VideoNotFoundExeption.class, () -> 
+        {
+        cultureMediaService.ListAllVideos();
         });
     }
 
     @Test
-    void when_FindAll_all_videos_should_be_returned_successfully() {
-        List<Video> videos = List.of( VideoA, VideoB, VideoC, VideoD, VideoE, VideoF);
+    void when_FindAll_all_videos_should_be_returned_successfully() throws VideoNotFoundExeption{
         
-        for ( Video video : videos ) {
-            cultureMediaService.add( video );
-        }
-
-        try 
-        {
-            List<Video> Videos = cultureMediaService.ListAllVideos();
-            assertEquals(6, Videos.size());
-        } catch (VideoNotFoundExeption e) {
-            assert(false);
-        }
+       
+            mockFindAll(List.of( VideoA, VideoB, VideoC, VideoD, VideoE, VideoF));
+            
+            List<Video> videos = cultureMediaService.ListAllVideos();
+            assertEquals(6, videos.size());
     }
 
     @Test
     void when_you_search_by_title_and_it_is_not_found_exception_VideoNotFoundExeption() {
+        mockFind(null, Collections.emptyList());
         assertThrows(VideoNotFoundExeption.class, () -> {
             cultureMediaService.find("better luck for the next");
         });
     }
 
     @Test
-    void when_you_search_by_title_and_you_are_successful() {
-        List<Video> videos = List.of(VideoA, VideoB, VideoC, VideoD, VideoE, VideoF);
-        
-        for ( Video video : videos ) {
-            cultureMediaService.add( video );
-        }
-
-        try {
-            List<Video> Videos = cultureMediaService.find("The");
-            assertEquals(3, Videos.size());
-            
-        } catch (VideoNotFoundExeption e) {
-            assert(false);
-        }    
-    }
-
-
-
-    @Test 
     void when_you_search_by_forDuration_and_it_is_not_found_exception_VideoNotFoundExeption() {
+        mockFindTime(null, null, Collections.emptyList());
         assertThrows(VideoNotFoundExeption.class, () -> {
-            cultureMediaService.find(0.002, 0.3);
+            cultureMediaService.find(0.1, 0.3);
         });
     }
 
-    @Test
-    void when_find_forDuration_should_be_returned_succesfully() {
-        List<Video> videos = List.of(VideoA, VideoB, VideoC, VideoD, VideoE, VideoF);
-        
-        for ( Video video : videos ) {
-            cultureMediaService.add( video );
-        }
 
-        try {
-            List<Video> Videos = cultureMediaService.find(3.3, 6.6);
-            assertEquals(4, Videos.size());
-        } catch (VideoNotFoundExeption e) {
-            assert(false);
-        }
+    @Test
+    void when_find_forDuration_should_be_returned_succesfully()  throws VideoNotFoundExeption{
+        mockFindTime(0.0, 7.7, List.of( VideoA, VideoB, VideoC, VideoD, VideoE, VideoF));
+        List<Video> videos = cultureMediaService.find(0.0, 2.3);
+        assertEquals(6, videos.size());
     }
+
+    @Test
+    void when_find_forTitle_should_be_returned_succesfully() throws VideoNotFoundExeption{
+
+        mockFind("Sea", List.of(VideoF));
+
+        List<Video> videos = cultureMediaService.find("Sea");
+        assertEquals(1, videos.size());
+        assertEquals(VideoF, videos.get(0));
+    }
+
+   
 
 }
