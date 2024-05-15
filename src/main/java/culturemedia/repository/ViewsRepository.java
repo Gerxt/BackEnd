f2368e68b509bd7469adc9e852d2f0600c0d02d8
Gerxt <german-2396@hotmail.com>
2024-05-15 03:20:56 -0500

    Adding repository impl

--- src/main/java/culturemedia/repository/ViewsRepository.java
+++ src/main/java/culturemedia/repository/ViewsRepository.java
@@ -1,6 +1,9 @@
 package culturemedia.repository;
 import culturemedia.model.View;
 
-public interface ViewsRepository {
-    View add(View view);
+public interface ViewsRepository 
+{
+
+    View save(View view);
+
 }
